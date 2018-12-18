d6ce7043c58004e7a56056c0f0459761ea5a106a
Dengshan Yuan <deyuan@microsoft.com>
2018-12-18 08:10:40 -0800

    test catalog

---
+++ test/testcatalog/App/CustomFunction/Upgrade/v1.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../Office.Runtime.js" />
+/// <reference path="../Excel.js" />
+function hideSync(num) {
+	return num + 11;
+}
+
+function changeSync(num) {
+	return num + 12;
+}
+
+function hideAsync(num) {
+	return num + 21;
+}
+
+function changeAsync(num) {
+	return num + 22;
+}
---
+++ test/testcatalog/App/CustomFunction/Upgrade/v2.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../Office.Runtime.js" />
+/// <reference path="../Excel.js" />
+function changeSync(num) {
+	return num + 112;
+}
+
+function showSync(num) {
+	return num + 13;
+}
+
+function changeAsync(num) {
+	return num + 222;
+}
+
+function showAsync(num) {
+	return num + 23;
+}
