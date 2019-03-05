fd127e417296e724c7e383cf9c24e273271ad482
Dengshan Yuan <deyuan@microsoft.com>
2019-03-04 17:07:15 -0800

    postmessage

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,17 @@
 
+webkit.messageHandlers.agaveserviceworker.postMessage('execute');
+
 self.addEventListener('install', event => {
 	console.log('install');
+	webkit.messageHandlers.agaveserviceworker.postMessage('install');
 });
 
 self.addEventListener('activate', event => {
 	console.log('active');
+	webkit.messageHandlers.agaveserviceworker.postMessage('activate');
 });
 
 self.addEventListener('fetch', event => {
 	console.log('fetch');
+	webkit.messageHandlers.agaveserviceworker.postMessage('fetch');
 });
